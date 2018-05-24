b183eccd232d108ce3cfa4d06587d4ec83b1674c
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-05-23 19:48:14 -0700

    custom implementation of brd.c -- brk.c implements brock devices

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+# initialize environment
+source ./environment-setup-i586-poky-linux
+
 # DEFAULT
 #qemu-system-i386 -gdb tcp::5528 -S -nographic -kernel ../bzImage-qemux86.bin -drive file=../core-image-lsb-sdk-qemux86.ext4,if=virtio -enable-kvm -net none -usb -localtime --no-reboot --append "root=/dev/vda rw console=ttyS0 debug".
 
